09028c46d9658daa13ccef19493094fce46dab6f
fiannsaa <73948568+fiannsaa@users.noreply.github.com>
2020-11-04 14:28:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,11 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
-const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
+const BotName = 'Fiannsaa 🤖'; // Nama Bot Whatsapp
+const instagramlu = 'https://instagram.com/fiannsaa'; // Nama Instagramlu cok
+const whatsapplu = '0856-5688-4416'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://chat.whatsapp.com/KLW3UlFfeaH36Ucm5zRfCz'; // OFFICIAL GRUP LU 2
-//
+const grupch1 = 'https://chat.whatsapp.com/Grvc9b7LCXEA3J2ehlN438'; // OFFICIAL GRUP LU 1
 const
 {
    WAConnection,
